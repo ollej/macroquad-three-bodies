4f31fb6843f87ca66985d3f1075761fbe2b671cb
Olle Wreede <olle@wreede.se>
2024-04-05 13:08:39 +0200

    Add test and some refactor

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ const GRAVITATIONAL_CONSTANT: f64 = 6.67430e-11;
 
 type Position = DVec2;
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Body {
     mass: f64,
     position: Position,
@@ -35,7 +35,7 @@ impl fmt::Display for Body {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Step {
     time: f64,
     step: u32,
@@ -43,8 +43,24 @@ struct Step {
 }
 
 impl Step {
+    fn new(first: Body, second: Body, third: Body) -> Self {
+        Step {
+            time: 0.0,
+            step: 0,
+            bodies: [first, second, third],
+        }
+    }
+
     fn update(&mut self) {
         self.bodies.iter_mut().for_each(|body| body.update());
+    }
+
+    fn next_step(self, time_step: f64) -> Self {
+        Step {
+            time: self.time + time_step,
+            step: self.step + 1,
+            bodies: self.bodies,
+        }
     }
 }
 
@@ -58,27 +74,19 @@ impl fmt::Display for Step {
     }
 }
 
-fn main() {
-    let mut first = Body::new(dvec2(0.3089693008, 0.4236727692));
-    let mut second = Body::new(dvec2(-0.5, 0.0));
-    let mut third = Body::new(dvec2(0.5, 0.0));
+fn simulate(first: Body, second: Body, third: Body, count: usize, time_step: f64) -> Vec<Step> {
+    let mut steps = Vec::<Step>::with_capacity(count);
 
-    let mut steps = Vec::<Step>::with_capacity(STEPS);
+    let mut step = Step::new(first, second, third);
 
-    for n in 0..STEPS {
-        let mut new_step = Step {
-            time: (n as f64) * TIME_STEP,
-            step: n as u32,
-            bodies: [first, second, third],
-        };
-
+    for n in 0..count {
         for i in 0..3 {
             for j in 0..3 {
                 if i == j {
                     continue;
                 }
-                let a = &new_step.bodies[j];
-                let mut b: Body = new_step.bodies[i];
+                let a = &step.bodies[j];
+                let mut b: Body = step.bodies[i];
 
                 let dx = a.position.x - b.position.x;
                 let dy: f64 = a.position.y - b.position.y;
@@ -88,22 +96,154 @@ fn main() {
                 let angle = dy.atan2(dx);
                 let fx = force * angle.cos();
                 let fy = force * angle.sin();
-                b.velocity.x += fx / b.mass * TIME_STEP;
-                b.velocity.y += fy / b.mass * TIME_STEP;
+                b.velocity.x += fx / b.mass * time_step;
+                b.velocity.y += fy / b.mass * time_step;
 
-                new_step.bodies[i] = b;
+                step.bodies[i] = b;
             }
         }
 
-        new_step.update();
-
-        [first, second, third] = new_step.bodies;
-
-        steps.push(new_step);
+        step.update();
+        steps.push(step);
+        step = step.next_step(time_step);
 
         // report current state
         if n % 1000 == 0 {
-            println!("{}", new_step);
+            println!("{}", step);
         }
+    }
+    steps
+}
+
+fn main() {
+    let first = Body::new(dvec2(0.3089693008, 0.4236727692));
+    let second = Body::new(dvec2(-0.5, 0.0));
+    let third = Body::new(dvec2(0.5, 0.0));
+
+    simulate(first, second, third, STEPS, TIME_STEP);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simulate() {
+        let first = Body::new(dvec2(0.3089693008, 0.4236727692));
+        let second = Body::new(dvec2(-0.5, 0.0));
+        let third = Body::new(dvec2(0.5, 0.0));
+
+        let steps = simulate(first, second, third, 5, TIME_STEP);
+
+        assert_eq!(
+            steps,
+            vec![
+                Step {
+                    time: 0.0,
+                    step: 0,
+                    bodies: [
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.30896930081402885, 0.423672769120293),
+                            velocity: dvec2(2.8057636600640765e-11, -1.5941407464626255e-10)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(-0.49999999996558936, 9.282862773097892e-12),
+                            velocity: dvec2(6.882126950562697e-11, 1.8565725546195783e-11)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.4999999999515605, 7.042417455003339e-11),
+                            velocity: dvec2(-9.687890610626773e-11, 1.4084834910006679e-10)
+                        }
+                    ]
+                },
+                Step {
+                    time: 0.5,
+                    step: 1,
+                    bodies: [
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.30896930084208646, 0.4236727689608789),
+                            velocity: dvec2(5.611527324112887e-11, -3.188281493901134e-10)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(-0.4999999998967681, 2.784858832017373e-11),
+                            velocity: dvec2(1.3764253902280134e-10, 3.713145109415168e-11)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.49999999985468163, 2.1127252369801421e-10),
+                            velocity: dvec2(-1.937578122639302e-10, 2.8169669829596167e-10)
+                        }
+                    ]
+                },
+                Step {
+                    time: 1.0,
+                    step: 2,
+                    bodies: [
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.3089693008841729, 0.4236727687217578),
+                            velocity: dvec2(8.417290996131172e-11, -4.782422243291407e-10)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(-0.49999999979353615, 5.569717664298761e-11),
+                            velocity: dvec2(2.0646380856307043e-10, 5.569717664562777e-11)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.49999999970936326, 4.2254504753977065e-10),
+                            velocity: dvec2(-2.906367185243821e-10, 4.225450476835129e-10)
+                        }
+                    ]
+                },
+                Step {
+                    time: 1.5,
+                    step: 3,
+                    bodies: [
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.3089693009402882, 0.42367276840292967),
+                            velocity: dvec2(1.1223054680103672e-10, -6.376562995609326e-10)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(-0.4999999996558936, 9.28286277441797e-11),
+                            velocity: dvec2(2.752850781379816e-10, 7.426290220238417e-11)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.4999999995156055, 7.042417462190449e-10),
+                            velocity: dvec2(-3.8751562493901825e-10, 5.633933973585484e-10)
+                        }
+                    ]
+                },
+                Step {
+                    time: 2.0,
+                    step: 4,
+                    bodies: [
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.3089693010104323, 0.42367276800439446),
+                            velocity: dvec2(1.402881838001512e-10, -7.970703751830775e-10)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(-0.49999999948384044, 1.3924294162727017e-10),
+                            velocity: dvec2(3.4410634775908215e-10, 9.282862776618096e-11)
+                        },
+                        Body {
+                            mass: 1.0,
+                            position: dvec2(0.4999999992734082, 1.0563626199274932e-9),
+                            velocity: dvec2(-4.843945315592333e-10, 7.042417474168966e-10)
+                        }
+                    ]
+                }
+            ]
+        );
     }
 }
